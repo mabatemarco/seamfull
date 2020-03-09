b90e5c4b17cd5a67a3e42ea5e9ef66b05d8f039d
mabatemarco <mabatemarco928@gmail.com>
2020-03-08 22:24:21 -0400

    little snowball

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,9 +17,9 @@ class UsersController < ApplicationController
   # POST /users
   def create
     @user = User.new(user_params)
-
     if @user.save
-      render json: @user, status: :created, location: @user
+      @token = encode({ user_id: @user.id, username: @user.username })
+      render json: { user: @user, token: @token }, status: :created, location: @user
     else
       render json: @user.errors, status: :unprocessable_entity
     end
@@ -39,7 +39,7 @@ class UsersController < ApplicationController
     @user.destroy
   end
 
-  private
+
   private
 
   def set_user
